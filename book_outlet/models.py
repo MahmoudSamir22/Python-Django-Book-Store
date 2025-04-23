21ebed347f619f6efbaf34f90a860e51c39bf696
Mahmoud Samir <menatosamir55@gmail.com>
2025-04-23 21:54:14 +0200

    author template-migrations

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -2,15 +2,32 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.text import slugify
 
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code =models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    def __str__(self):
+        return self.full_name()
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(default=5, validators=[MinValueValidator(0), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
+
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
