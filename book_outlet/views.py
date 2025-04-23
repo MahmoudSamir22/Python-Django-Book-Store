5c516656ac123b597873926f2c181f1b5275ced2
Mahmoud Samir <menatosamir55@gmail.com>
2025-04-23 02:46:36 +0200

    add templates

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import Book
 # Create your views here.
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, "index.html", {
+        "books": books
+    })
+
+def book_detail(request, id):
+    book = Book.objects.get(pk=id)
+    print(book)
+    return render(request, "book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
+    })
